4e9395598206012da6495f55331b64d3b1f151dd
unknown <T119365@GMA-395424.ad.git.cginet>
2015-04-05 17:57:13 -0400

    add new project

---
+++ MavenApp/src/main/java/com/cgi/maven/MavenApp/App.java
@@ -0,0 +1,17 @@
+package com.cgi.maven.MavenApp;
+
+import org.slf4j.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       // System.out.println( "Hello World!" );
+    	Logger logger = LoggerFactory.getLogger(App.class);
+    	logger.info("Hello World");
+    }
+}
